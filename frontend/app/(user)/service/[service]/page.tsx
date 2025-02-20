2548412ac2199d42e207168e53b496254e255122
abdelkderboukert <abdelkaderboukart@gmail.com>
2025-02-20 07:56:52 +0100

    feat: add console log for service parameter in service page component

--- frontend/app/(user)/service/[service]/page.tsx
+++ frontend/app/(user)/service/[service]/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const Page = ({ params }: { params: Promise<{ service: string }> }) => {
   const { service } = React.use(params);
+  console.log(service);
   return (
     <section className="h-max min-h-screen w-full grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-3">
       <div className="h-[50dvh] lg:h-full w-full bg-black lg:col-span-1"></div>
