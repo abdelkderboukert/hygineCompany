894c532bc32cab62c89fafa0591da9ac5d3137b9
abdelkderboukert <abdelkaderboukart@gmail.com>
2025-04-09 09:56:45 +0100

    chore: fix typo in comment for Header import in layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import * as motion from "motion/react-client";
 {
-  /*import Header from "@/components/Header";*/
+  /*import Header from "@/components/Header";k*/
 }
 import "./globals.css";
 
